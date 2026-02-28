e4fb9adfdb23fbcefb27dd0c0dc2e1f59706b1f4
Ainoa <aimmolina2@gmail.com>
2026-02-28 18:28:49 +0100

    caso 1 ready

--- README.md
+++ README.md
@@ -15,6 +15,21 @@ Todos los resultados se mostrarán por consola desde el index.html del proyecto.
 
 # CASO 1. Caso de un cliente particular
 
+Volcamos la info facilitada en el ejercicio (interface y reservas) en el archivo `main.ts``
+
+Creamos la `class ReservaParticular` que incluye la info de reservas (pasamos el array de reservas) y el precio adicional por cada persona extra (que según el ejercicio será de 40 euros)
+
+Calculamos el precio:
+
+- primero del subtotal (el acumulado + precio por tipo de habitacion x noche + precio adicional por las personas, que lo calculamos siempre restando -1, de manera que si no hay personas extra se suma 0)
+- y después del total
+
+Para evitar que nos de infinitos decimales recurrimos al .toFixed(2).
+
+Pasamos los resultados por consola.
+
+<img src=images/caso-1.png alt="caso 1">
+
 # CASO 2. Caso de un touroperador
 
 # DESAFÍO
--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ interface Reserva {
   noches: number;
 }
 
-const reservas = [
+const reservas: Reserva[] = [
   {
     tipoHabitacion: "standard",
     pax: 1,
@@ -23,3 +23,47 @@ const reservas = [
     noches: 1,
   },
 ];
+
+class ReservaParticular {
+  reservas: Reserva[];
+  precioAdicional: number;
+
+  constructor(reservas: Reserva[]) {
+    this.reservas = reservas;
+    this.precioAdicional = 40;
+  }
+  calcularPrecio(tipoHabitacion: string): number {
+    switch (tipoHabitacion) {
+      case "standard":
+        return 100;
+      case "suite":
+        return 150;
+      default:
+        return 0;
+    }
+  }
+
+  get subTotal() {
+    return Number(
+      this.reservas
+        .reduce((acc, reserva) => {
+          return (
+            acc +
+            this.calcularPrecio(reserva.tipoHabitacion) * reserva.noches +
+            this.precioAdicional * (reserva.pax - 1)
+          );
+        }, 0)
+        .toFixed(2),
+    );
+  }
+
+  get total() {
+    return Number((this.subTotal * 1.21).toFixed(2)); // +IVA
+  }
+}
+
+const reservaParticular = new ReservaParticular(reservas);
+console.log("---------------- CASO 1: Reserva Particular ----------------");
+console.log(reservaParticular);
+console.log("Subtotal: ", reservaParticular.subTotal);
+console.log("Total: ", reservaParticular.total);
